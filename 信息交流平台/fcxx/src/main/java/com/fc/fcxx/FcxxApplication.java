ccf47756b58be81f6960a42edcb86b2d26c9403f
yunmoya <yunmoya@users.noreply.github.com>
2019-10-31 09:00:59 +0800

    2019-10-31

---
+++ 信息交流平台/fcxx/src/main/java/com/fc/fcxx/FcxxApplication.java
@@ -0,0 +1,13 @@
+package com.fc.fcxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcxxApplication.class, args);
+	}
+
+}
